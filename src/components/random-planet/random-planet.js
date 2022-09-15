341e38d312f5f9aef5ee51e3ce14e59cd22ac292
SorryLegacy <alexremnev2@gmail.com>
2022-09-15 17:21:32 +0300

    Add error handler, loading

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,67 +1,96 @@
 import React, { Component } from 'react';
 
-import SwapiService from "../../services/api-client";
+
+import ErrorIndicator from "../error-indicator";
+import Spinner from '../spinner';
+import SwapiService from '../../services/api-client';
 
 import './random-planet.css';
+
 
 export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
-  }
+    planet: {},
+    loading: true,
+    error: false,
+  };
 
   constructor() {
     super();
     this.updatePlanet();
   }
 
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({
+      planet,
+      loading: false
+    });
+  };
+
   updatePlanet() {
     const id = Math.floor(Math.random()*25)+2;
-    console.log(id)
-    this.swapiService.getPlanet(id).then((planet) =>{
-        this.setState({
-          id,
-          name:planet.name,
-          population: planet.population,
-          rotationPeriod: planet.rotation_period,
-          diameter: planet.diameter
-        });
-    });
+    this.swapiService
+        .getPlanet(id)
+        .then(this.onPlanetLoaded)
+        .catch(this.onError);
   }
 
   render() {
+    const { planet, loading, error } = this.state;
 
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator/> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="random planet"/>
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
-
     );
   }
 }
+
+const PlanetView = ({ planet }) => {
+
+  const { id, name, population,
+    rotationPeriod, diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+           alt="planet"/>
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
--- src/services/api-client.js
+++ src/services/api-client.js
@@ -11,28 +11,72 @@ export default class SwapiService {
 
     async getAllPeople() {
         const res = await this.getResource(`/people/`);
-        return res.results;
+        return res.results.map(this._transformPerson);
     }
 
     async getAllPlanets() {
         const res = await this.getResource(`/planets/`);
-        return res.results;
+        return res.results.map(this._transformPlanet);
     }
 
     async getAllStarships() {
         const res = await this.getResource('/starships/');
-        return res.results;
+        return res.results.map(this._transformStarship);
     }
 
-    getStarship(id) {
-        return this.getResource(`/starships/${id}/`);
+    async getStarship(id) {
+        const starship = this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starship);
     }
 
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}/`);
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet)
+
     }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(person)
+    }
+
+    _extraId(item) {
+        const regExp = /\/([0-9]*)\/$/;
+        return item.url.match(regExp)[1];
+    }
+
+    _transformPlanet(planet) {
+
+        return {
+            id: this._extraId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
+    }
+
+    _transformStarship(starship){
+        return {
+            id: this._extraId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+
+    _transformPerson(people) {
+        return {
+            id: this._extraId(people),
+            name: people.name,
+            gender: people.gender,
+            birthYear: people.birthYear,
+            eyeColor: people.eyeColor
+        }
     }
 }
