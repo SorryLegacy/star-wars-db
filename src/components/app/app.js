06da380bbe3b5a26ec20600eea97680d1fece910
SorryLegacy <alexremnev2@gmail.com>
2022-09-23 20:44:45 +0300

    add context Api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,21 @@ import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorIndicator from '../error-indicator';
+import {PersonList} from "../sw-components";
+import PersonDetails from "../sw-components/person-details";
+
+import SwapiService from "../../services/api-client";
+
+import {SwapiServiceProvider} from "../swapi-service-context";
 
 import './app.css';
-import {PersonList} from "../sw-components";
-import {PersonDetails} from "../sw-components";
+import ErrorHandler from "../error-handler";
+
+
 
 export default class App extends Component {
+
+  swapiService = new SwapiService()
 
   state = {
     showRandomPlanet: true,
@@ -30,12 +39,16 @@ export default class App extends Component {
       null;
 
     return (
-      <div className="stardb-app">
-        <Header />
-        { planet }
-        <PersonList />
-        <PersonDetails itemId={11}/>
-      </div>
+        <ErrorHandler>
+          <SwapiServiceProvider value={this.swapiService}>
+          <div className="stardb-app">
+            <Header />
+            { planet }
+            <PersonList />
+            <PersonDetails itemId={11}/>
+          </div>
+            </SwapiServiceProvider>
+        </ErrorHandler>
     );
   }
 }
